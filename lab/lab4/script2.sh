0c28e2e1eb325cd37db91677ef043439faf61446
Johnny205 <grabmar97@gmail.com>
2019-05-13 19:58:40 +0200

    lab4 added

---
+++ lab/lab4/script2.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#Skrypt wyswietlajacy zawartosc pliku wraz z numerami linii np:
+awk '{print NR "," $0}' $1
+
+#Skrypt wyswietlajacy zawartosc pliku, ale tylko linie 5-10
+awk '{if(NR>4 && NR<11) print NR  $0}' $1
+
+#Skrypt zamieniajacy male litery na duze w liniach zawierajacych cyfry
+awk '{if($NR ~ /[0-9]+/) print NR toupper($0) }' $1
